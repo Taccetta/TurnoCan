bc18cf9b710d9cd2fc98adbf410e0d6fef2b196e
Taccetta <n.taccetta@alumno.um.edu.ar>
2024-10-24 01:11:31 -0300

    password set

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import time
-from PyQt5.QtWidgets import QApplication, QMessageBox
+import hashlib
+from PyQt5.QtWidgets import QApplication, QMessageBox, QInputDialog, QLineEdit
 from main_window import MainWindow
 from database import init_db, close_db_connections, verify_database_integrity
 from backup import create_auto_backup, restore_from_auto_backup, try_restore_database
@@ -8,7 +9,7 @@ import os
 
 def handle_database_error(e):
     print(f"Error al inicializar o verificar la base de datos: {e}")
-    close_db_connections()  # Cerrar todas las conexiones existentes
+    close_db_connections()  
     
     success, timestamp = try_restore_database()
     if success:
@@ -20,8 +21,34 @@ def handle_database_error(e):
         QMessageBox.critical(None, "Error", "No se pudo restaurar la base de datos después de varios intentos.")
         return False
 
+def prompt_password():
+    hashed_password = 'a05ba560427a528100410c8553ffb49df7493d8a6308922c200fd81c538cf8fe'
+    
+    while True:
+        input_password, ok = QInputDialog.getText(
+            None, 
+            'Autenticación Requerida', 
+            'Ingrese la contraseña:', 
+            QLineEdit.Password
+        )
+
+        if not ok:
+            return False
+
+        if input_password:
+            hashed_input = hashlib.sha256(input_password.encode()).hexdigest()
+            if hashed_input == hashed_password:
+                return True
+            else:
+                QMessageBox.warning(None, "Error de Autenticación", "Contraseña incorrecta. Intente nuevamente.")
+        else:
+            QMessageBox.warning(None, "Error", "Por favor, ingrese una contraseña.")
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    
+    # if not prompt_password():
+    #     sys.exit(0) 
     
     try:
         init_db()
